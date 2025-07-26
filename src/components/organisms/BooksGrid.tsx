3da4f04bdc507807dff1ddb3e1e55e64ade1c130
GmoWilliams <guillermowilliams01@gmail.com>
2025-07-26 16:36:23 -0700

    :sparkles: Now listing book covers and their main information

---
+++ src/components/organisms/BooksGrid.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react"
+import BookCard from "../atoms/BookCard"
+
+interface Book {
+    id: string,
+    author: {
+        name: string,
+        key: string,
+    },
+    coverId: string,
+    title: string,
+    year: string,
+}
+
+const BooksGrid = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('https://openlibrary.org/subjects/love.json?limit=30')
+        .then(response => response.json())
+        .then(json => {
+            const result = json.works || []
+            setData(result)
+        })
+        .catch(error => console.error(`There was an error: ${error}`))
+    }, [])
+
+    const sample = data.slice(0, 10)
+
+    const books = sample.map((item: any) => {
+        return {
+        id: item.key || 'NA',
+        author: {
+            name: item?.authors[0]?.name || 'NA',
+            key: item?.authors[0]?.key || 'NA',
+        },
+        coverId: item.cover_edition_key || 'NA',
+        title: item.title || 'NA',
+        year: item.first_publish_year || 'NA',
+    }})
+
+    console.log(`This is books: ${JSON.stringify(books, null, 2)}`)
+
+    return (
+        <div>
+            {books.map((book: Book) => (
+                <BookCard
+                    id={book.id}
+                    author={book.author}
+                    title={book.title}
+                    year={book.year} 
+                    // Open Library ID
+                    format={'olid'}
+                    // from "cover_edition_key"
+                    bookId={book.coverId}
+                    size={'L'}
+                />
+            ))}
+            
+        </div>
+    )
+}
+
+export default BooksGrid
