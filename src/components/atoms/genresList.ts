54a5e39dd32ccf2358712d791630960abbe6c312
GmoWilliams <guillermowilliams01@gmail.com>
2025-07-26 22:56:36 -0700

    :sparkles: Added Genre List

---
+++ src/components/atoms/genresList.ts
@@ -0,0 +1,51 @@
+export const genreList = [
+    {
+        value: 'love',
+        text: 'Love',
+    },
+    {
+        value: 'horror',
+        text: 'Horror',
+    },
+    {
+        value: 'mystery_and_detective_stories',
+        text: 'Mystery',
+    },
+    {
+        value: 'poetry',
+        text: 'Poetry',
+    },
+    {
+        value: 'nutrition',
+        text: 'Nutrition',
+    },
+    {
+        value: 'dogs',
+        text: 'Dogs',
+    },
+    {
+        value: 'psychology',
+        text: 'Psychology',
+    },
+    {
+        value: 'programming',
+        text: 'Programming',
+    },
+    {
+        value: 'religion',
+        text: 'Religion',
+    },
+]
+
+/*
+
+Genres to add in the future:
+
+romance
+short_stories
+mental_health
+exercise
+nutrition
+cooking
+autobiography
+*/
